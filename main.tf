6be69a2c765613b661803a301b6aa699cd921201
He Guimin <guimin.hgm@alibaba-inc.com>
2020-06-18 15:00:23 +0800

    delete vpc

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ output "regions" {
   value = data.alicloud_regions.default.ids
 }
 
-resource "alicloud_vpc" "default" {
-  cidr_block = "172.16.0.0/16"
-  name = "terraform-demo"
-}
+//resource "alicloud_vpc" "default" {
+//  cidr_block = "172.16.0.0/16"
+//  name = "terraform-demo"
+//}
