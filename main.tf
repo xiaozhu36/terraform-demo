888ee473836465edb9568a83277e481f8fd69805
He Guimin <guimin.hgm@alibaba-inc.com>
2020-06-18 14:55:05 +0800

    create a new vpc

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ data "alicloud_regions" "default" {}
 output "regions" {
   value = data.alicloud_regions.default.ids
 }
+
+resource "alicloud_vpc" "default" {
+  cidr_block = "172.16.0.0/16"
+  name = "terraform-demo"
+}
